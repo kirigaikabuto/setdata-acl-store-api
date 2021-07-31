390cd722436d57b3146b29b4aba0c0ce42b93286
Yerassyl <tleugazy98@gmail.com>
2021-07-31 13:08:28 +0600

    commits

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kirigaikabuto/setdata-acl-store-api
 
 go 1.13
+
+require (
+	github.com/kirigaikabuto/setdata-acl v0.0.0-20210731065537-12bfcc7252b6 // indirect
+	github.com/kirigaikabuto/setdata-common v1.0.1-0.20210731064419-35e1e27617d0
+)
--- main.go
+++ main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"github.com/djumanoff/amqp"
+	setdata_acl "github.com/kirigaikabuto/setdata-acl"
+	setdata_common "github.com/kirigaikabuto/setdata-common"
+)
+
 func main() {
+	rabbitConfig := amqp.Config{
+		Host:     "localhost",
+		Port:     5672,
+		LogLevel: 5,
+	}
+	serverConfig := amqp.ServerConfig{
+		ResponseX: "response",
+		RequestX:  "request",
+	}
+	sess := amqp.NewSession(rabbitConfig)
+	err := sess.Connect()
+	if err != nil {
+		panic(err)
+		return
+	}
+	srv, err := sess.Server(serverConfig)
+	if err != nil {
+		panic(err)
+		return
+	}
+	config := setdata_acl.PostgresConfig{
+		Host:     "localhost",
+		Port:     5432,
+		User:     "setdatauser",
+		Password: "123456789",
+		Database: "setdata",
+		Params:   "sslmode=disable",
+	}
+	//role
+	postgreRoleStore, err := setdata_acl.NewPostgresRoleStore(config)
+	if err != nil {
+		panic(err)
+		return
+	}
+	roleService := setdata_acl.NewRoleService(postgreRoleStore)
+	roleAmqpEndpoints := setdata_acl.NewRoleAmqpEndpoints(setdata_common.NewCommandHandler(roleService))
+	srv.Endpoint("role.create", roleAmqpEndpoints.MakeCreateRoleAmqpEndpoint())
+	srv.Endpoint("role.get", roleAmqpEndpoints.MakeGetRoleAmqpEndpoint())
+	srv.Endpoint("role.list", roleAmqpEndpoints.MakeListRoleAmqpEndpoint())
+	srv.Endpoint("role.delete", roleAmqpEndpoints.MakeDeleteRoleAmqpEndpoint())
+
+	//permissions
+	postgrePermissionStore, err := setdata_acl.NewPostgresPermissionStore(config)
+	if err != nil {
+		panic(err)
+		return
+	}
+	permissionService := setdata_acl.NewPermissionService(postgrePermissionStore)
+	permissionAmqpEndpoints := setdata_acl.NewPermissionAmqpEndpoints(setdata_common.NewCommandHandler(permissionService))
+	srv.Endpoint("permission.create", permissionAmqpEndpoints.MakeCreatePermissionAmqpEndpoint())
+	srv.Endpoint("permission.get", permissionAmqpEndpoints.MakeGetPermissionAmqpEndpoint())
+	srv.Endpoint("permission.list", permissionAmqpEndpoints.MakeListPermissionAmqpEndpoint())
+	srv.Endpoint("permission.delete", permissionAmqpEndpoints.MakeDeletePermissionAmqpEndpoint())
+	err = srv.Start()
+	if err != nil {
+		panic(err)
+		return
+	}
 
 }
