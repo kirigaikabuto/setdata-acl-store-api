137a0504579fafc6471911360b162cb89f27d219
Yerassyl <tleugazy98@gmail.com>
2021-07-31 18:48:58 +0600

    commits

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/djumanoff/amqp v1.0.6
-	github.com/kirigaikabuto/setdata-acl v0.0.0-20210731073202-df041047a195
+	github.com/kirigaikabuto/setdata-acl v0.0.0-20210731124342-f991cf59c3ed
 	github.com/kirigaikabuto/setdata-common v1.0.1-0.20210731073120-f240c1ec93b0
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/djumanoff/amqp"
 	setdata_acl "github.com/kirigaikabuto/setdata-acl"
 	setdata_common "github.com/kirigaikabuto/setdata-common"
@@ -60,6 +61,25 @@ func main() {
 	srv.Endpoint("permission.get", permissionAmqpEndpoints.MakeGetPermissionAmqpEndpoint())
 	srv.Endpoint("permission.list", permissionAmqpEndpoints.MakeListPermissionAmqpEndpoint())
 	srv.Endpoint("permission.delete", permissionAmqpEndpoints.MakeDeletePermissionAmqpEndpoint())
+
+	//role permissions
+	postgreRolePermissionStore, err := setdata_acl.NewPostgresRolePermissionStore(config)
+	if err != nil {
+		panic(err)
+		return
+	}
+	rolePermissionService := setdata_acl.NewRolePermissionService(
+		postgreRolePermissionStore,
+		postgreRoleStore,
+		postgrePermissionStore,
+	)
+	rolePermissionAmqpEndpoints := setdata_acl.NewRolePermissionAmqpEndpoints(setdata_common.NewCommandHandler(rolePermissionService))
+	srv.Endpoint("role_permission.create", rolePermissionAmqpEndpoints.MakeCreateRolePermissionAmqpEndpoint())
+	srv.Endpoint("role_permission.list", rolePermissionAmqpEndpoints.MakeListRolePermissionAmqpEndpoint())
+	srv.Endpoint("role_permission.get", rolePermissionAmqpEndpoints.MakeGetRolePermissionAmqpEndpoint())
+	srv.Endpoint("role_permission.delete", rolePermissionAmqpEndpoints.MakeDeleteRolePermissionAmqpEndpoint())
+
+	fmt.Println(postgreRolePermissionStore)
 	err = srv.Start()
 	if err != nil {
 		panic(err)
